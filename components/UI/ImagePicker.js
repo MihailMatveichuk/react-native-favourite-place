77fdb2c9fe5f869dd7ed36926e61fb53e5f398bb
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2024-05-14 14:03:38 +0300

    feat: add image preview

--- components/UI/ImagePicker.js
+++ components/UI/ImagePicker.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { Alert, Button, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, Button, Image, StyleSheet, Text, View } from 'react-native';
 import {
   launchCameraAsync,
   useCameraPermissions,
   PermissionStatus,
 } from 'expo-image-picker';
+import { Colors } from '../../constants/styles';
 
 export function ImagePicker() {
   const [cameraPermissionInformation, requestPermission] =
     useCameraPermissions();
+  const [imageUri, setImageUri] = useState(null);
 
   const verifyPermissions = async () => {
     if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
@@ -41,13 +43,45 @@ export function ImagePicker() {
       aspectRatio: [16, 9],
       quality: 0.5,
     });
-    console.log(image.assets[0].uri);
+
+    setImageUri(image.assets[0].uri);
   };
 
   return (
     <View>
-      <View></View>
+      <View style={styles.imageContainer}>
+        {imageUri ? (
+          <Image source={{ uri: imageUri }} style={styles.image} />
+        ) : (
+          <Text style={styles.imageText}>No image selected</Text>
+        )}
+      </View>
       <Button title="Take a photo" onPress={handleImagePicked} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+    width: '100%',
+    height: 300,
+    borderRadius: 4,
+    overflow: 'hidden',
+    backgroundColor: Colors.primary100,
+  },
+
+  imageText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: Colors.primary900,
+  },
+
+  image: {
+    width: '95%',
+    height: '95%',
+  },
+});
